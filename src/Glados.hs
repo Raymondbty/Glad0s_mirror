08204944c8ac9d7ce218ec57ef076101da67105c
Victor Doucet <victor.doucet@epitech.eu>
2024-01-01 17:42:14 +0100

    [FIX] Ast Define String

--- src/Glados.hs
+++ src/Glados.hs
@@ -52,8 +52,8 @@ data Ast = Define String Ast
 sexprToAST :: SExpr -> Maybe Ast
 sexprToAST (SInt i) = Just $ IntLiteral i
 sexprToAST (SSymbol i) = Just $ StringLiteral i
-sexprToAST (SList [SSymbol "define", SSymbol symbol, SInt i]) = Just $ Define (StringLiteral symbol) (IntLiteral i)
-sexprToAST (SList [SSymbol "define", SSymbol symbol, SSymbol s]) = Just $ Define (StringLiteral symbol) (StringLiteral s)
+sexprToAST (SList [SSymbol "define", SSymbol symbol, SInt i]) = Just $ Define symbol (IntLiteral i)
+sexprToAST (SList [SSymbol "define", SSymbol symbol, SSymbol s]) = Just $ Define symbol (StringLiteral s)
 sexprToAST _ = Nothing
 
 evalAST :: Ast -> Maybe Ast
