e70e779a594441908aefe6a09472e5b6e0e2039c
Victor Doucet <victor.doucet@epitech.eu>
2024-01-06 19:08:30 +0100

    [ADD] parseString func to parse string with space

--- src/Parser.hs
+++ src/Parser.hs
@@ -41,6 +41,12 @@ parseAst (x:xs) | x == '(' && null xs == False && (head $ reverse xs) == ')' = p
                 | isStringNumber (x : xs) = IntLiteral (read (x : xs) :: Int)
                 | otherwise = StringLiteral (x : xs)
 
+parseString :: String -> (String, String)
+parseString [] = ([], [])
+parseString (x:xs) | x == '"' = ([], xs)
+                   | otherwise = let (str, rest) = (parseString xs) in
+                                 (x : str, rest)
+
 parseIntSym :: String -> Ast
 parseIntSym ('-':xs) | isStringNumber xs = IntLiteral (((read xs) :: Int) * (-1))
 parseIntSym str      | isStringNumber str = IntLiteral ((read str) :: Int)
@@ -48,6 +54,10 @@ parseIntSym str      = Symbol str
 
 parse :: String -> [Ast]
 parse [] = []
+parse (' ':xs) = parse xs
+parse ('"':xs) = case parseString xs of
+                    ([], _) -> []
+                    (str, rest) -> (StringLiteral str) : (parse rest)
 parse str = case firstWord str of
                 ([], _) -> []
-                (word, rest) -> [parseIntSym word] ++ (parse rest)
+                (word, rest) -> (parseIntSym word) : (parse rest)
