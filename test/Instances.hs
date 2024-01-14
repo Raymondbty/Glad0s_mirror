7aa553f9e34ccb47e9284fe6a726fa84236d51cb
hugo mouraud <hugo.mouraud@epitech.eu>
2024-01-14 20:48:58 +0100

    [ADD] add new instance for tests (Value) and (Instructions)

--- test/Instances.hs
+++ test/Instances.hs
@@ -10,6 +10,16 @@ module Instances (SExpr, Ast) where
 import Types
 import Glados
 
+instance Eq Operator where
+    ADD == ADD = True
+    SUB == SUB = True
+    MUL == MUL = True
+    DIV == DIV = True
+    MOD == MOD = True
+    EQUAL == EQUAL = True
+    LESS == LESS = True
+    _ == _ = False
+
 instance Eq SExpr where
   (SList a) == (SList b) = a == b
   (SSymbol a) == (SSymbol b) = a == b
@@ -21,3 +31,16 @@ instance Eq Ast where
   StringLiteral a == StringLiteral b = a == b
   Define sym1 expr1 == Define sym2 expr2 = sym1 == sym2 && expr1 == expr2
   _ == _ = False
+
+instance Eq Value where
+    (IntVM x) == (IntVM y) = x == y
+    (BoolVM x) == (BoolVM y) = x == y
+
+instance Eq Instruction where
+    (Push v1) == (Push v2) = v1 == v2
+    (CallOp op1) == (CallOp op2) = op1 == op2
+    Ret == Ret = True
+    (JUMPIFFALSE i1) == (JUMPIFFALSE i2) = i1 == i2
+    (JUMP i1) == (JUMP i2) = i1 == i2
+    _ == _ = False
+
