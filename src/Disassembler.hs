29e1e6d23faafe7dc947a00f7b200625bf64be4b
Victor Doucet <victor.doucet@epitech.eu>
2024-01-21 17:47:41 +0100

    [ADD] Disassembler

--- src/Disassembler.hs
+++ src/Disassembler.hs
@@ -8,12 +8,48 @@
 module Disassembler (disassemble) where
 
 import Control.Exception
+import Data.Bits
 import qualified Data.ByteString as BS
 import Data.Word
 import System.Exit
 
-getAssembler :: [Word8] -> Either String String
-getAssembler _ = Left "str"
+continueReading :: String -> [Word8] -> Maybe String
+continueReading str xs =
+    case getAssembler xs of
+        Just [] -> Just $ str
+        Just code -> Just $ str ++ "\n" ++ code
+        Nothing -> Nothing
+
+bytesToInt :: Word8 -> Word8 -> Word8 -> Word8 -> Int
+bytesToInt b1 b2 b3 b4 = (fromIntegral b1)
+                       + (fromIntegral b2 `shiftL` 8)
+                       + (fromIntegral b3 `shiftL` 16)
+                       + (fromIntegral b4 `shiftL` 24)
+
+readInstr :: String -> [Word8] -> Maybe String
+readInstr instr (b1:b2:b3:b4:xs) =
+    continueReading (instr ++ " " ++ (show $ bytesToInt b1 b2 b3 b4)) xs
+readInstr _ _ = Nothing
+
+readCall :: [Word8] -> Maybe String
+readCall (0x00:xs) = continueReading "Push Add\nCall" xs
+readCall (0x01:xs) = continueReading "Push Sub\nCall" xs
+readCall (0x02:xs) = continueReading "Push Mul\nCall" xs
+readCall (0x03:xs) = continueReading "Push Div\nCall" xs
+readCall (0x04:xs) = continueReading "Push Mod\nCall" xs
+readCall (0x05:xs) = continueReading "Push Eq\nCall" xs
+readCall (0x06:xs) = continueReading "Push Less\nCall" xs
+readCall (0x07:xs) = continueReading "Push Fact\nCall" xs
+readCall _ = Nothing
+
+getAssembler :: [Word8] -> Maybe String
+getAssembler [] = Just []
+getAssembler (0x00:xs) = readInstr "Push" xs
+getAssembler (0x01:xs) = readCall xs
+getAssembler (0x02:xs) = continueReading "Ret" xs
+getAssembler (0x03:xs) = readInstr "JumpIfFalse" xs
+getAssembler (0x04:xs) = readInstr "Jump" xs
+getAssembler _ = Nothing
 
 disassemble :: String -> IO ()
 disassemble file = do
@@ -22,5 +58,5 @@ disassemble file = do
         Left e ->
             putStrLn ("Exception: " ++ show e) >> exitWith (ExitFailure 84)
         Right binary -> case getAssembler $ BS.unpack binary of
-                            Left err -> putStrLn $ "Error: " ++ err
-                            Right str -> putStrLn str
+                            Just code -> putStrLn code
+                            Nothing -> putStrLn $ "Error: binary corrupted"
