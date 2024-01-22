db317bece2e54d36cbcad7b4bfc5247edafedc65
Victor Doucet <victor.doucet@epitech.eu>
2024-01-22 17:03:12 +0100

    [ADD] recursive functions in functions

--- src/Parser.hs
+++ src/Parser.hs
@@ -16,13 +16,18 @@ firstWord (x:xs)
     | otherwise = let (str, rest) = firstWord xs in
                   (x : str, rest)
 
-parseAsBracket :: String -> Maybe (String, String)
-parseAsBracket [] = Nothing
-parseAsBracket (x:xs)
-    | x == '}'  = Just ([], xs)
-    | otherwise = case parseAsBracket xs of
-        Just (str, rest) -> Just (x : str, rest)
-        Nothing -> Nothing
+parseAsBracket :: Int -> String -> Maybe (String, String)
+parseAsBracket 0 rest = Just ([], rest)
+parseAsBracket _ [] = Nothing
+parseAsBracket i ('{':xs) = case parseAsBracket (i + 1) xs of
+                                Just (str, rest) -> Just ('{' : str, rest)
+                                Nothing -> Nothing
+parseAsBracket i ('}':xs) = case parseAsBracket (i - 1) xs of
+                                Just (str, rest) -> Just ('}' : str, rest)
+                                Nothing -> Nothing
+parseAsBracket i (x:xs) = case parseAsBracket i xs of
+                                Just (str, rest) -> Just (x : str, rest)
+                                Nothing -> Nothing
 
 firstBracket :: String -> Maybe String
 firstBracket [] = Just []
@@ -33,9 +38,8 @@ firstBracket _ = Nothing
 parseAsParent :: String -> Maybe (String, String)
 parseAsParent [] = Just ([], [])
 parseAsParent (' ': xs) = parseAsParent xs
-parseAsParent (x:xs)
-    | x == '(' = Just ([], xs)
-    | otherwise = case parseAsParent xs of
+parseAsParent ('(':xs) = Just ([], xs)
+parseAsParent (x:xs) = case parseAsParent xs of
         Just (str, rest) -> Just (x : str, rest)
         Nothing -> Nothing
 
@@ -45,9 +49,9 @@ checkLetter x = (x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z' )
 parseWord :: String -> Maybe (String, String, Bool)
 parseWord [] = Nothing
 parseWord (' ': xs) = parseWord xs
+parseWord (')':xs) = Just ([], xs, True)
+parseWord (',':xs) = Just ([], xs, False)
 parseWord (x:xs)
-    | x == ')' = Just ([], xs, True)
-    | x == ',' = Just ([], xs, False)
     | checkLetter x =  case parseWord xs of
         Just (str, rest, res) -> Just (x : str, rest, res)
         Nothing -> Nothing
@@ -62,13 +66,13 @@ parseParams str = case parseWord str of
         Nothing -> Nothing
     Nothing -> Nothing
 
-parseFunc :: String -> Maybe Ast
+parseFunc :: String -> Maybe (Ast, String)
 parseFunc [] = Nothing
 parseFunc str = case parseAsParent str of
     Just (name, rest) -> case parseParams rest of
         Just (args, rest1) -> case firstBracket rest1 of
-            Just rest2 -> case parseAsBracket rest2 of
-                Just (str2, rest3) -> Just $ Call2 name args []
+            Just rest2 -> case parseAsBracket 1 rest2 of
+                Just (str2, rest3) -> Just (Call2 name args (parse str2), rest3)
                 Nothing -> Nothing
             Nothing -> Nothing
         Nothing -> Nothing
@@ -83,6 +87,6 @@ parse ('\r':xs) = parse xs
 parse ('\n':xs) = parse xs
 parse str = case firstWord str of
                 ("func", rest) -> case parseFunc rest of
-                    Just ast -> [ast]
+                    Just (ast, rest1) -> ast : (parse rest1)
                     Nothing -> []
                 _ -> []
